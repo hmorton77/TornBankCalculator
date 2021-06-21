8e8edeb96238557f83e2905306a5dc93b4dc3e99
Harold Morton <61100228+hmorton77@users.noreply.github.com>
2021-06-21 15:53:51 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2> TORN(tm) Bank interest Calculator</h2>
+      <div>
+        <p> My Torn API is: </p>
+      </div>
+      {/* "1w": "36.31",
+		      "2w": "42.16",
+		      "1m": "48.98",
+		      "2m": "48.68",
+		      "3m": "50.85" */}
+      <div>
+        <p> number of merits in bank interest: </p>
+      </div>
+      <div>
+        <p> I have the Torn city banking Bonus Block: </p>
+      </div>
+      <div>
+        <p> I work at a 10* oil rig: </p>
+      </div>
     </div>
   );
 }
---
+++ src/components/resultBox/resultBox.js
@@ -0,0 +1,2 @@
+// by the end of [time period] you will make a profit of [money]
+// with a profit of [money/time period] per day
